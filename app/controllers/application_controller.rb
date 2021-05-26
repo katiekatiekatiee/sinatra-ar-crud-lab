d9eb5c8353f0e1b261d2cccac61948ce36a48449
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-05-25 21:38:37 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,25 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+ 
+  get '/articles/new' do
+    erb :new
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    @article = Article.new(params)
+    redirect :"/articles/#{@article.id}"
+  end
+ 
+
 end
